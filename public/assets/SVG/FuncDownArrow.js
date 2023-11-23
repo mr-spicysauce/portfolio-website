c7cf70ec1182564889ebb428b7d806f9967892c9
mr_spicysauce <mr_spicysauce@yahoo.com>
2023-11-23 22:45:59 +0000

    minimum viable product

---
+++ public/assets/SVG/FuncDownArrow.js
@@ -0,0 +1,22 @@
+export default function MainHeader() {
+    return (
+        <div id="FuncDownArrow">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13"
+                height="9"
+                viewBox="0 0 13 9"
+                fill="none"
+                className="AboutMeFuncColapse"
+            >
+                <path
+                    d="M12 1.5L6.5 7.5L1 1.5"
+                    stroke="#464F5F"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+            </svg>
+        </div>
+    )
+}
---
+++ public/assets/SVG/GodotIndent.js
@@ -0,0 +1,22 @@
+export default function MainHeader() {
+    return (
+        <div id="GodotIndentDiv">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="11"
+                height="13"
+                viewBox="0 0 11 13"
+                fill="none"
+                id="GodotIndent"
+            >
+                <path
+                    d="M1 1L7 6.5L1 12M10 1V12"
+                    stroke="#464F5F"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+            </svg>
+        </div>
+    )
+}
