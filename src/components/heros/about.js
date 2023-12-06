6b073a698f41cdbe7b9f71c26ed226dad6c4d4b2
mr_spicysauce <mr_spicysauce@yahoo.com>
2023-12-06 17:54:33 +0000

    ✏️ Wrote about me section

--- src/components/heros/about.js
+++ src/components/heros/about.js
@@ -12,6 +12,18 @@ export const AboutHero = () => {
         })
     }, [])
 
+    //I did not write this I took it from stack overflow :)
+    function getAge(dateString) {
+        var today = new Date()
+        var birthDate = new Date(dateString)
+        var age = today.getFullYear() - birthDate.getFullYear()
+        var m = today.getMonth() - birthDate.getMonth()
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--
+        }
+        return age
+    }
+
     return (
         <div id="about" className="about">
             <header id="Main4Header">
@@ -104,8 +116,8 @@ export const AboutHero = () => {
                         <span id="GodotPrint">&nbsp;print</span>
                         <span className="AboutMeSymbols">(</span>
                         <span className="AboutmeText">
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            "Hello, I'm Jack, a passionate game developer and
+                            programmer from the UK. At the age of
                         </span>
                     </div>
 
@@ -113,48 +125,46 @@ export const AboutHero = () => {
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            {getAge('2006-01-05')}, I immersed myself in the
+                            world of game development and programming for the
+                            past {getAge('2021-09-09')}
                         </span>
                     </div>
                     <div className="line">
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            years, constantly improving my skills and pushing
+                            boundaries.
+                        </span>
+                    </div>
+                    <div className="line">
+                        <GodotIndent />
+                        <GodotIndent />
+                        <span className="AboutmeText"></span>
+                    </div>
+                    <div className="line">
+                        <GodotIndent />
+                        <GodotIndent />
+                        <span className="AboutmeText">
+                            My journey began as a young kid crafting games in
+                            Scratch and delving into the intricacies of
                         </span>
                     </div>
                     <div className="line">
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            setting up servers and mini-games in Minecraft,
+                            Garry’s Mod, and Fortnite. During my time in
                         </span>
                     </div>
                     <div className="line">
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
-                        </span>
-                    </div>
-                    <div className="line">
-                        <GodotIndent />
-                        <GodotIndent />
-                        <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
-                        </span>
-                    </div>
-                    <div className="line">
-                        <GodotIndent />
-                        <GodotIndent />
-                        <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            college, I took the leap to create my own games,
+                            laying the foundation for a promising future
                         </span>
                     </div>
 
@@ -162,48 +172,51 @@ export const AboutHero = () => {
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            in game development.
+                        </span>
+                    </div>
+                    <div className="line">
+                        <GodotIndent />
+                        <GodotIndent />
+                        <span className="AboutmeText"></span>
+                    </div>
+                    <div className="line">
+                        <GodotIndent />
+                        <GodotIndent />
+                        <span className="AboutmeText">
+                            While I'm not behind the code, I indulge in the
+                            world of competitive tactical shooters like
                         </span>
                     </div>
                     <div className="line">
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            Valorant and Overwatch 2, fueling my competitive
+                            drive. On the flip side, I have a deep
                         </span>
                     </div>
                     <div className="line">
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            appreciation for narrative-driven games such as
+                            Detroit Become Human, Uncharted and the
                         </span>
                     </div>
                     <div className="line">
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            Life is Strange series. And, when I'm in the mood
+                            for a change of pace, you'll catch me
                         </span>
                     </div>
                     <div className="line">
                         <GodotIndent />
                         <GodotIndent />
                         <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
-                        </span>
-                    </div>
-                    <div className="line">
-                        <GodotIndent />
-                        <GodotIndent />
-                        <span className="AboutmeText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Proin vitae orci non neque fringilla
+                            enjoying rhythm games like Beat Saber and Osu.
                         </span>
                     </div>
                     <div className="line">
@@ -222,10 +235,6 @@ export const AboutHero = () => {
                             &nbsp;get_song_playing_on_spotify
                         </span>
                         <span className="AboutMeSymbols">():</span>
-                    </div>
-
-                    <div className="line">
-                        <GodotIndent />
                     </div>
 
                     <div className="line">
